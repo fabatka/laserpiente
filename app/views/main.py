644a5766777d4e89eaaecb957b82f4ca15e996b2
Levente Dologh <ldologh@gmail.com>
2020-04-02 23:12:53 +0200

    Introduce base template and home page html

--- app/views/main.py
+++ app/views/main.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-bp = Blueprint('main', __name__)
+bp = Blueprint('main', __name__, template_folder='templates')
 
 
 @bp.route('/')
 @bp.route('/home')
 def home():
-    return 'Hello!'
+    return render_template('home.html')
