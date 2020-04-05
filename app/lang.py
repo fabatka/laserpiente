6355f5cb4b989a4c15d964030f45926cf83d86b6
Levente Dologh <ldologh@gmail.com>
2020-04-05 10:18:09 +0200

    Add lang.py that defines mapping between db and human readable text

---
+++ app/lang.py
@@ -0,0 +1,10 @@
+pronoun_map_db_hr = {
+    'singular1': 'yo',
+    'singular2': 'tú',
+    'singular3': 'él/ella/usted',
+    'plural1': 'nosotros',
+    'plural2': 'vosotros',
+    'plural3': 'ellos/ellas/ustedes'
+}
+
+pronoun_map_hr_db = {v: k for k, v in pronoun_map_db_hr.items()}
