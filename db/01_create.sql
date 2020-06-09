65ece9dab2afa91242aa73a88e2bd0598ce57986
Levente Dologh <ldologh@gmail.com>
2020-06-09 22:45:23 +0200

    Restructure table holding verbs

--- db/01_create.sql
+++ db/01_create.sql
@@ -2,20 +2,24 @@ create schema laserpiente;
 create user dev with password 'devpass';
 grant all on schema laserpiente to dev;
 
-create table laserpiente.v_conj_ind_pres
+create table laserpiente.verbos
 (
-    infinitivo varchar not null
-        constraint v_conj_ind_pres_pk
-            primary key,
-    singular1  varchar not null,
-    singular2  varchar not null,
-    singular3  varchar not null,
-    plural1    varchar not null,
-    plural2    varchar not null,
-    plural3    varchar not null
+	infinitivo varchar not null
+		constraint v_conj_ind_pres_pk
+			primary key,
+	gerundio varchar,
+	participio varchar,
+	modo varchar,
+	tiempo varchar,
+	singular1 varchar not null,
+	singular2 varchar not null,
+	singular3 varchar not null,
+	plural1 varchar not null,
+	plural2 varchar not null,
+	plural3 varchar not null
 );
 
-grant select on laserpiente.v_conj_ind_pres to dev;
+grant select on laserpiente.verbos to dev;
 
 create table laserpiente.frase
 (
@@ -43,7 +47,7 @@ create index ejercicio_quiz_index
 grant select on laserpiente.ejercicio to dev;
 
 
-insert into laserpiente.v_conj_ind_pres (infinitivo, singular1, singular2, singular3, plural1, plural2, plural3)
+insert into laserpiente.verbos (infinitivo, singular1, singular2, singular3, plural1, plural2, plural3)
 values ('comer', 'como', 'comes', 'come', 'comemos', 'coméis', 'comen'),
        ('ir', 'voy', 'vas', 'va', 'vamos', 'vais', 'van'),
        ('pedir', 'pido', 'pides', 'pide', 'pedimos', 'pedís', 'piden');
