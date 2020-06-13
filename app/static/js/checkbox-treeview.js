c08e2bd53dd6f51a273cd34a016cde724941382b
Levente Dologh <ldologh@gmail.com>
2020-06-14 00:13:31 +0200

    Reformat checkbox-treeview.js code

--- app/static/js/checkbox-treeview.js
+++ app/static/js/checkbox-treeview.js
@@ -1,67 +1,65 @@
 function checkboxTreeview() {
 
-  $('input[type="checkbox"]').change(checkboxChanged);
+    $('input[type="checkbox"]').change(checkboxChanged);
 
-  function checkboxChanged() {
-      let $this = $(this),
-          checked = $this.prop("checked"),
-          container = $this.parent(),
-          siblings = container.siblings();
+    function checkboxChanged() {
+        let $this = $(this),
+            checked = $this.prop("checked"),
+            container = $this.parent(),
+            siblings = container.siblings();
 
-      container.find('input[type="checkbox"]')
-    .prop({
-        indeterminate: false,
-        checked: checked
-    })
-    .siblings('label')
-    .removeClass('custom-checked custom-unchecked custom-indeterminate')
-    .addClass(checked ? 'custom-checked' : 'custom-unchecked');
+        container.find('input[type="checkbox"]')
+            .prop({
+                indeterminate: false,
+                checked: checked
+            })
+            .siblings('label')
+            .removeClass('custom-checked custom-unchecked custom-indeterminate')
+            .addClass(checked ? 'custom-checked' : 'custom-unchecked');
 
     checkSiblings(container, checked);
   }
 
-  function checkSiblings($el, checked) {
-      let parent = $el.parent().parent(),
-          all = true,
-          indeterminate = false;
+    function checkSiblings($el, checked) {
+        let parent = $el.parent().parent(),
+            all = true,
+            indeterminate = false;
 
-      $el.siblings().each(function() {
-      return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
-    });
+        $el.siblings().each(function () {
+            return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+        });
 
-    if (all && checked) {
-      parent.children('input[type="checkbox"]')
-      .prop({
-          indeterminate: false,
-          checked: checked
-      })
-      .siblings('label')
-      .removeClass('custom-checked custom-unchecked custom-indeterminate')
-      .addClass(checked ? 'custom-checked' : 'custom-unchecked');
+        if (all && checked) {
+            parent.children('input[type="checkbox"]')
+                .prop({
+                    indeterminate: false,
+                    checked: checked
+                })
+                .siblings('label')
+                .removeClass('custom-checked custom-unchecked custom-indeterminate')
+                .addClass(checked ? 'custom-checked' : 'custom-unchecked');
 
-      checkSiblings(parent, checked);
+            checkSiblings(parent, checked);
+        } else if (all && !checked) {
+            indeterminate = parent.find('input[type="checkbox"]:checked').length > 0;
+
+            parent.children('input[type="checkbox"]')
+                .prop("checked", checked)
+                .prop("indeterminate", indeterminate)
+                .siblings('label')
+                .removeClass('custom-checked custom-unchecked custom-indeterminate')
+                .addClass(indeterminate ? 'custom-indeterminate' : (checked ? 'custom-checked' : 'custom-unchecked'));
+
+            checkSiblings(parent, checked);
+        } else {
+            $el.parents("li").children('input[type="checkbox"]')
+                .prop({
+                    indeterminate: true,
+                    checked: false
+                })
+                .siblings('label')
+                .removeClass('custom-checked custom-unchecked custom-indeterminate')
+                .addClass('custom-indeterminate');
+        }
     }
-    else if (all && !checked) {
-      indeterminate = parent.find('input[type="checkbox"]:checked').length > 0;
-
-      parent.children('input[type="checkbox"]')
-      .prop("checked", checked)
-      .prop("indeterminate", indeterminate)
-      .siblings('label')
-      .removeClass('custom-checked custom-unchecked custom-indeterminate')
-      .addClass(indeterminate ? 'custom-indeterminate' : (checked ? 'custom-checked' : 'custom-unchecked'));
-
-      checkSiblings(parent, checked);
-    }
-    else {
-      $el.parents("li").children('input[type="checkbox"]')
-      .prop({
-          indeterminate: true,
-          checked: false
-      })
-      .siblings('label')
-      .removeClass('custom-checked custom-unchecked custom-indeterminate')
-      .addClass('custom-indeterminate');
-    }
-  }
 };
