3dc96b8d9855f4bf485566f5c2efdc8d7a9adeae
Levente Dologh <ldologh@gmail.com>
2020-07-03 11:30:33 +0200

    Add security headers to home page

--- app/views/home.py
+++ app/views/home.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+import secrets
+
+from flask import Blueprint, render_template, make_response
+
+from app.static.utils import add_security_headers
 
 bp = Blueprint('home', __name__, template_folder='templates')
 
 
 @bp.route('/', methods=['GET'])
 def home():
-    return render_template('home.html')
+    nonce = secrets.token_urlsafe()
+    return add_security_headers(make_response(render_template('home.html', nonce=nonce), 200), nonce)
